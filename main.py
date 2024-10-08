e1e71924113511d770bd5b0577807f50ee9f6ab0
Taruu <taruumax@gmail.com>
2024-10-08 08:40:35 +0300

    space

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
+
 ## TODO change python-telegram-bot tp Telethon
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-    
+
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.PHOTO & filters.ChatType.GROUPS, handle_photo))
-    
+
     application.add_handler(CommandHandler("set_qe_threshold", set_qe_threshold))
     application.add_handler(CommandHandler("set_tag_threshold", set_tag_threshold))
     application.add_handler(CommandHandler("add_banned_tag", add_banned_tag))
     application.add_handler(CommandHandler("remove_banned_tag", remove_banned_tag))
-    
+
     application.run_polling()
+
 
 if __name__ == '__main__':
     main()
